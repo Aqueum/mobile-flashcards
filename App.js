4fabb33bdee50c5bc55dc2a5bbf22d9a2b0cdb09
Aqueum <martin.currie@aqueum.com>
2018-05-01 19:47:29 +0100

    feat: Add styled header for all views

--- App.js
+++ App.js
@@ -1,8 +1,27 @@
 import React from 'react';
 import DeckListView from './components/DeckListView';
+import styled from 'styled-components';
+
+const Page = styled.View`
+  flex: 1;
+  padding-top: 30px;
+  background-color: white;
+`;
+
+const Header = styled.Text`
+  color: black;
+  text-align: left;
+  margin-left: 5;
+  font-size: 22;
+`;
 
 export default class App extends React.Component {
   render() {
-    return <DeckListView />;
+    return (
+      <Page>
+        <Header>Mobile Flashcards</Header>
+        <DeckListView />
+      </Page>
+    );
   }
 }
