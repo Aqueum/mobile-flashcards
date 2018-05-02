686447e52141179b460315d9520378a9cdd718a2
Aqueum <martin.currie@aqueum.com>
2018-05-02 23:10:41 +0100

    feat: Add text box

--- components/DeckAdd.js
+++ components/DeckAdd.js
@@ -1,22 +1,49 @@
 import React, { Component } from 'react';
-import { TouchableOpacity } from 'react-native';
+import { TouchableOpacity, TextInput } from 'react-native';
 import styled from 'styled-components';
+import { saveDeckTitle } from '../utils/api';
 
 const Page = styled.View`
   flex: 1;
 `;
 
-const Title = styled.Text`
+const Prompt = styled.Text`
   color: black;
-  font-size: 20;
-  text-align: center;
+  font-size: 18;
+  text-align: left;
+  margin-left: 5;
+  margin-top: 5;
+`;
+
+const Submit = styled.Text`
+  color: blue;
+  font-size: 18;
+  text-align: right;
+  margin-right: 5;
+  margin-top: 5;
 `;
 
 class DeckAdd extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { title: '' };
+  }
+
   render() {
     return (
       <Page>
-        <Title>Add a new deck</Title>
+        <Prompt>Enter new deck title:</Prompt>
+        <TextInput
+          style={{
+            height: 40,
+            borderColor: 'gray',
+            borderWidth: 1,
+            margin: 10,
+            padding: 10
+          }}
+          onChangeText={title => this.setState({ title })}
+          value={this.state.title}
+        />
       </Page>
     );
   }
