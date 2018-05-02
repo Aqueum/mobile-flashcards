2dd7b428273e8416c01e1da0f134142441e97886
Aqueum <martin.currie@aqueum.com>
2018-05-02 15:34:30 +0100

    feat: Open DeskListView sending dummy data

---
+++ containers/DeckList.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import DeckListView from '../components/DeckListView';
+
+const decks = [
+  { key: 'old deck', count: 3 },
+  { key: 'new deck', count: 2 },
+  { key: 'green deck', count: 3 },
+  { key: 'blue deck', count: 2 },
+  { key: 'tinker deck', count: 3 },
+  { key: 'taylor deck', count: 2 },
+  { key: 'soldier deck', count: 3 },
+  { key: 'spy deck', count: 2 },
+  { key: 'next deck', count: 2 },
+  { key: 'other deck', count: 2 }
+];
+
+export default function DeckList() {
+  return <DeckListView decks={decks} />;
+}
