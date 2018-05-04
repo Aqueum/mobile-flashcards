a7b1cb5e32689d3245bb7f5cc5b324513f0e19bf
Aqueum <martin.currie@aqueum.com>
2018-05-04 12:14:33 +0100

    feat: Add front page to simplify App

---
+++ components/FrontPage.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import styled from 'styled-components';
+import { StackNavigator } from 'react-navigation';
+import DeckAdd from './components/DeckAdd';
+import DeckListView from './components/DeckListView';
+import DeckView from './components/DeckView';
+import { YellowBox, TouchableOpacity } from 'react-native';
+import { FontAwesome } from '@expo/vector-icons';
+import { createStore } from 'redux';
+
+//https://github.com/react-navigation/react-navigation/issues/3956#issuecomment-380648083
+YellowBox.ignoreWarnings([
+  'Warning: isMounted(...) is deprecated',
+  'Module RCTImageLoader'
+]);
+
+const Page = styled.View`
+  flex: 1;
+  padding-top: 30px;
+  background-color: white;
+`;
+
+const Header = styled.Text`
+  color: black;
+  text-align: left;
+  margin-left: 5;
+  font-size: 22;
+`;
+
+const Plus = styled.Text`
+  color: gray;
+  margin-right: 5;
+  font-size: 36;
+`;
+
+const MainNavigator = StackNavigator({
+  Home: {
+    screen: DeckListView
+  },
+  Deck: {
+    screen: DeckView
+  },
+  AddDeck: {
+    screen: DeckAdd
+  },
+  initialRouteName: 'Home'
+});
+
+DeckListView.navigationOptions = ({ navigation }) => ({
+  title: 'Decks',
+  headerRight: (
+    <TouchableOpacity
+      onPress={() => {
+        navigation.navigate('AddDeck');
+      }}
+    >
+      <Plus>+</Plus>
+    </TouchableOpacity>
+  )
+});
+
+export default class FrontPage extends React.Component {
+  render() {
+    return (
+      <Page>
+        <MainNavigator />
+      </Page>
+    );
+  }
+}
