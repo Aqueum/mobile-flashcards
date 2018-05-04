a426c6443296c810367d184ff3b38c0366e70c2e
Aqueum <martin.currie@aqueum.com>
2018-05-04 13:19:10 +0100

    feat: Add reducers

--- reducers/index.js
+++ reducers/index.js
@@ -1,3 +1,56 @@
+import {
+  REQUEST_DECKS,
+  RECEIVE_DECKS,
+  REQUEST_DECK,
+  RECEIVE_DECK,
+  REQUEST_NEW_DECK,
+  RECEIVE_NEW_DECK
+} from '../actions';
+
+export default function rootReducer(
+  state = {
+    isFetching: false,
+    decks: [],
+    deck: {}
+  },
+  action
+) {
+  switch (action.type) {
+    case REQUEST_DECKS:
+      return Object.assign({}, state, {
+        isFetching: true
+      });
+    case REQUEST_DECK:
+      return Object.assign({}, state, {
+        isFetching: true
+      });
+    case REQUEST_NEW_DECK:
+      return Object.assign({}, state, {
+        isFetching: true
+      });
+    case RECEIVE_DECKS:
+      return Object.assign({}, state, {
+        isFetching: false,
+        decks: action.decks,
+        lastUpdated: action.receivedAt
+      });
+    case RECEIVE_DECK:
+      return Object.assign({}, state, {
+        isFetching: false,
+        deck: action.deck,
+        lastUpdated: action.receivedAt
+      });
+    case RECEIVE_NEW_DECK:
+      return Object.assign({}, state, {
+        isFetching: false,
+        deck: action.deck,
+        lastUpdated: action.receivedAt
+      });
+  }
+}
+
+/*
+
 // the root reducer - that combines all reducers into one
 // that can be called by the main App.js
 
@@ -6,3 +59,5 @@ import { combineReducers } from 'redux';
 const rootReducer = combineReducers({});
 
 export default rootReducer;
+
+*/
