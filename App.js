fa047b2633c1cea8ef1a2978810b81d50bb93369
Aqueum <martin.currie@aqueum.com>
2018-05-04 17:17:44 +0100

    feat: Add middleware

--- App.js
+++ App.js
@@ -1,12 +1,22 @@
-import React from 'react';
-import { createStore } from 'redux';
-import Provider from 'react-redux';
-import rootReducer from './reducers';
+import React, { Component } from 'react';
+import thunkMiddleware from 'redux-thunk';
+import { createLogger } from 'redux-logger';
+import { createStore, applyMiddleware, compose } from 'redux';
+import { Text } from 'react-native';
+import { Provider } from 'react-redux';
+import Reducers from './reducers';
 import FrontPage from './components/FrontPage';
 
-const store = createStore(rootReducer);
+const loggerMiddleware = createLogger();
 
-export default class App extends React.Component {
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+  Reducers,
+  composeEnhancers(applyMiddleware(thunkMiddleware, loggerMiddleware))
+);
+
+export default class App extends Component {
   render() {
     return (
       <Provider store={store}>
