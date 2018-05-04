c3f757dd6f91c10e2f641f6385bbe237c109f880
Aqueum <martin.currie@aqueum.com>
2018-05-04 12:07:05 +0100

    feat: Start root reducer

---
+++ reducers/index.js
@@ -0,0 +1,11 @@
+// the root reducer - that combines all reducers into one
+// that can be called by the main index.js
+
+import { combineReducers } from 'redux';
+
+
+const rootReducer = combineReducers({
+
+});
+
+export default rootReducer;
