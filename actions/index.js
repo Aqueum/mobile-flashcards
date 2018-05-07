e59d472df4035faa3c8893694eec53438bdbd364
Aqueum <martin.currie@aqueum.com>
2018-05-07 12:35:03 +0100

    fix: Simplify action-reducers without requests

--- actions/index.js
+++ actions/index.js
@@ -4,13 +4,6 @@ import { DECKS, initialiseData } from '../utils/data';
 // getDecks: return all of the decks
 // along with their titles, questions, and answers.
 
-export const REQUEST_DECKS = 'REQUEST_DECKS';
-function requestDecks() {
-  return {
-    type: REQUEST_DECKS
-  };
-}
-
 export const RECEIVE_DECKS = 'RECEIVE_DECKS';
 function receiveDecks(json) {
   return {
@@ -20,29 +13,8 @@ function receiveDecks(json) {
   };
 }
 
-export function getDecks() {
-  return function(dispatch) {
-    dispatch(requestDecks());
-    return AsyncStorage.getItem(DECKS)
-      .then(initialiseData)
-      .then(
-        response => response.json(),
-        error => console.log('An error occurred.', error)
-      )
-      .then(json => dispatch(receiveDecks(json)));
-  };
-}
-
 // getDeck: take in a single id argument
 // and return the deck associated with that id.
-
-export const REQUEST_DECK = 'REQUEST_DESK';
-function requestDeck(id) {
-  return {
-    type: REQUEST_DECK,
-    id
-  };
-}
 
 export const RECEIVE_DECK = 'RECEIVE_DECK';
 function receiveDeck(id, json) {
@@ -54,54 +26,17 @@ function receiveDeck(id, json) {
   };
 }
 
-export function getDeck(id) {
-  return function(dispatch) {
-    dispatch(requestDeck(id));
-    return AsyncStorage.getItem(DECKS.id)
-      .then(
-        response => response.json(),
-        error => console.log('An error occurred.', error)
-      )
-      .then(json => dispatch(receiveDeck(id, json)));
-  };
-}
-
 // saveDeckTitle: take in a single title argument
 // and add it to the decks.
 
-export const REQUEST_NEW_DECK = 'REQUEST_NEW_DESK';
-function requestNewDeck(title) {
-  return {
-    type: REQUEST_NEW_DECK,
-    title
-  };
-}
-
-export const RECEIVE_NEW_DECK = 'RECEIVE_NEW_DECK';
+export const NEW_DECK = 'NEW_DECK';
 function receiveNewDeck(title, json) {
   return {
-    type: RECEIVE_NEW_DECK,
+    type: NEW_DECK,
     title,
     deck: json,
     receivedAt: Date.now()
   };
-}
-
-export function saveDeckTitle(title) {
-  return function(dispatch) {
-    dispatch(requestNewDeck(title));
-    return AsyncStorage.mergeItem(
-      DECKS,
-      JSON.stringify({
-        [title]: 'test'
-      })
-    );
-  }
-    .then(
-      response => response.json(),
-      error => console.log('An error occurred.', error)
-    )
-    .then(json => dispatch(receiveNewDeck(title, json)));
 }
 
 // addCardToDeck: take in two arguments, title and card,
--- reducers/index.js
+++ reducers/index.js
@@ -1,48 +1,25 @@
-import {
-  REQUEST_DECKS,
-  RECEIVE_DECKS,
-  REQUEST_DECK,
-  RECEIVE_DECK,
-  REQUEST_NEW_DECK,
-  RECEIVE_NEW_DECK
-} from '../actions';
+import { RECEIVE_DECKS, RECEIVE_DECK, NEW_DECK } from '../actions';
 
 export default function Reducers(
   state = {
-    isFetching: false,
     decks: [],
     deck: {}
   },
   action
 ) {
   switch (action.type) {
-    case REQUEST_DECKS:
-      return Object.assign({}, state, {
-        isFetching: true
-      });
-    case REQUEST_DECK:
-      return Object.assign({}, state, {
-        isFetching: true
-      });
-    case REQUEST_NEW_DECK:
-      return Object.assign({}, state, {
-        isFetching: true
-      });
     case RECEIVE_DECKS:
       return Object.assign({}, state, {
-        isFetching: false,
         decks: action.decks,
         lastUpdated: action.receivedAt
       });
     case RECEIVE_DECK:
       return Object.assign({}, state, {
-        isFetching: false,
         deck: action.deck,
         lastUpdated: action.receivedAt
       });
-    case RECEIVE_NEW_DECK:
+    case NEW_DECK:
       return Object.assign({}, state, {
-        isFetching: false,
         deck: action.deck,
         lastUpdated: action.receivedAt
       });
