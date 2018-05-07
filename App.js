0fd575f77088381056a9e466cab6976657a6e9b7
Aqueum <martin.currie@aqueum.com>
2018-05-07 12:33:06 +0100

    fix: Attempt to suppress react-navigation isMounted warning

--- App.js
+++ App.js
@@ -2,11 +2,17 @@ import React, { Component } from 'react';
 import thunkMiddleware from 'redux-thunk';
 import { createLogger } from 'redux-logger';
 import { createStore, applyMiddleware, compose } from 'redux';
-import { Text } from 'react-native';
+import { Text, YellowBox } from 'react-native';
 import { Provider } from 'react-redux';
 import Reducers from './reducers';
 import FrontPage from './components/FrontPage';
 // import AsyncTrial from './components/AsyncTrial';
+
+//https://github.com/react-navigation/react-navigation/issues/3956#issuecomment-380648083
+YellowBox.ignoreWarnings([
+  'Warning: isMounted(...) is deprecated',
+  'Module RCTImageLoader'
+]);
 
 const loggerMiddleware = createLogger();
 
