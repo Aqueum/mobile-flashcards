73ca12b88ef3152d9a91c048e3aa9a003b750926
Aqueum <martin.currie@aqueum.com>
2018-05-07 23:43:07 +0100

    feat: Build quiz view

--- components/QuizView.js
+++ components/QuizView.js
@@ -12,6 +12,12 @@ const Title = styled.Text`
   margin-top: 30;
 `;
 
+const Enumeration = styled.Text`
+  color: grey;
+  font-size: 14;
+  text-align: center;
+`;
+
 const Button = styled.TouchableOpacity`
   background: white;
   border-radius: 3px;
@@ -27,26 +33,69 @@ const ButtonTxt = styled.Text`
 
 class QuizView extends Component {
   state = {
-    page: 1,
-    showAnswer: false
+    page: 0,
+    showAnswer: false,
+    score: 0
   };
   render() {
+    const { page, showAnswer, score } = this.state;
     const { params } = this.props.navigation.state;
     const item = params ? params.item : null;
-    console.log(item.questions[1].question);
     return (
       <Page>
         <Title>It's a {item.title} Quiz</Title>
-        <Title>Q: {item.questions[this.state.page].question}</Title>
-        <Button>
-          <ButtonTxt
-            onPress={() => {
-              this.setState({ showAnswer: true });
-            }}
-          >
-            Show Answer?
-          </ButtonTxt>
-        </Button>
+        {page < item.questions.length ? (
+          <Page>
+            <Enumeration>
+              {page + 1}/{item.questions.length}
+            </Enumeration>
+            <Title>Q: {item.questions[page].question}</Title>
+            {showAnswer === true ? (
+              <Page>
+                <Title>A: {item.questions[page].answer}</Title>
+                <Button>
+                  <ButtonTxt
+                    onPress={() => {
+                      this.setState({
+                        showAnswer: false,
+                        score: score + 1,
+                        page: page + 1
+                      });
+                    }}
+                  >
+                    Correct
+                  </ButtonTxt>
+                </Button>
+                <Button>
+                  <ButtonTxt
+                    onPress={() => {
+                      this.setState({
+                        showAnswer: false,
+                        page: page + 1
+                      });
+                    }}
+                  >
+                    Incorrect
+                  </ButtonTxt>
+                </Button>
+              </Page>
+            ) : (
+              <Button>
+                <ButtonTxt
+                  onPress={() => {
+                    this.setState({ showAnswer: true });
+                  }}
+                >
+                  Show Answer?
+                </ButtonTxt>
+              </Button>
+            )}
+          </Page>
+        ) : (
+          <Title>
+            You scored {Math.round(score / item.questions.length * 100)}%
+          </Title>
+        )}
       </Page>
     );
   }
