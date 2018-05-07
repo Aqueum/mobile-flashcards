08ce031c46c692ea77cafeb611161c53864387aa
Aqueum <martin.currie@aqueum.com>
2018-05-07 17:24:19 +0100

    fix: Remove removed reducers import

--- App.js
+++ App.js
@@ -4,7 +4,6 @@ import { createLogger } from 'redux-logger';
 import { createStore, applyMiddleware, compose } from 'redux';
 import { Text, YellowBox } from 'react-native';
 import { Provider } from 'react-redux';
-import Reducers from './reducers';
 import FrontPage from './components/FrontPage';
 // import AsyncTrial from './components/AsyncTrial';
 
@@ -19,7 +18,6 @@ const loggerMiddleware = createLogger();
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
-  Reducers,
   composeEnhancers(applyMiddleware(thunkMiddleware, loggerMiddleware))
 );
 
