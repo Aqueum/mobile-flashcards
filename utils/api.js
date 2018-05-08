f5453d177f9b17e27c85c0affe5c07571f1b0715
Aqueum <martin.currie@aqueum.com>
2018-05-08 22:54:26 +0100

    feat: getDecks from async storage

--- utils/api.js
+++ utils/api.js
@@ -1,7 +1,12 @@
 import { AsyncStorage } from 'react-native';
-import { DECKS } from './data';
+import { DECKS, starter } from './data';
 
 // To manage your AsyncStorage database, you'll want to create four different helper methods.
+
+// getDecks return all of the decks along with their titles, questions, and answers.
+export function getDecks() {
+  return AsyncStorage.getItem(DECKS);
+}
 
 // saveDeckTitle: take in a single title argument and add it to the decks.
 export function saveDeckTitle({ title, key }) {
