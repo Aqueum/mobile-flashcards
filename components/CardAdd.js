c41faac7ef0820a6b8bcaf951b8ad9729177c2c2
Aqueum <martin.currie@aqueum.com>
2018-05-09 17:36:27 +0100

    feat: Dispatch card

--- components/CardAdd.js
+++ components/CardAdd.js
@@ -3,6 +3,7 @@ import { TouchableOpacity, TextInput } from 'react-native';
 import styled from 'styled-components';
 import navigationOptions from 'react-navigation';
 import { connect } from 'react-redux';
+import { addCard } from '../actions';
 
 const Page = styled.View`
   flex: 1;
@@ -36,8 +37,9 @@ class CardAdd extends Component {
   state = { question: '', answer: '' };
 
   onSubmitEdit = () => {
+    const title = this.props.navigation.state.params.item.title;
     const card = { question: this.state.question, answer: this.state.answer };
-    console.log(card);
+    this.props.dispatch(addCard(title, card));
   };
 
   static navigationOptions = {
