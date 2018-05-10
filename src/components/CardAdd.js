be231d779f28bd71a130217f96d0bf01dead9bf8
Aqueum <martin.currie@aqueum.com>
2018-05-10 21:55:33 +0100

    refactor: Move CardAdd styles to style

--- src/components/CardAdd.js
+++ src/components/CardAdd.js
@@ -5,44 +5,7 @@ import navigationOptions from 'react-navigation';
 import { connect } from 'react-redux';
 import { addCard } from '../actions';
 import { addCardToDeck } from '../utils/api';
-
-// Styling
-
-const Page = styled.View`
-  flex: 1;
-`;
-
-const Prompt = styled.Text`
-  color: black;
-  font-size: 18;
-  text-align: left;
-  margin-left: 5;
-  margin-top: 5;
-`;
-
-const Input = styled.TextInput`
-  height: 40;
-  border-color: gray;
-  border-width: 1;
-  margin: 10px;
-  padding: 10px;
-`;
-
-const Submit = styled.Text`
-  color: blue;
-  font-size: 18;
-  text-align: right;
-  margin-right: 5;
-  margin-top: 5;
-`;
-
-const Reminder = styled.Text`
-  color: gray;
-  font-size: 14;
-  text-align: right;
-  margin-right: 5;
-  margin-top: 5;
-`;
+import { Page, Prompt, Input, Submit, Reminder } from '../style';
 
 // Page where new question and answer pair are entered
 // to be added to the deck from which this component is called
---
+++ src/style/index.js
@@ -0,0 +1,39 @@
+import styled from 'styled-components';
+
+// Styling
+
+export const Page = styled.View`
+  flex: 1;
+`;
+
+export const Prompt = styled.Text`
+  color: black;
+  font-size: 18;
+  text-align: left;
+  margin-left: 5;
+  margin-top: 5;
+`;
+
+export const Input = styled.TextInput`
+  height: 40;
+  border-color: gray;
+  border-width: 1;
+  margin: 10px;
+  padding: 10px;
+`;
+
+export const Submit = styled.Text`
+  color: blue;
+  font-size: 18;
+  text-align: right;
+  margin-right: 5;
+  margin-top: 5;
+`;
+
+export const Reminder = styled.Text`
+  color: gray;
+  font-size: 14;
+  text-align: right;
+  margin-right: 5;
+  margin-top: 5;
+`;
