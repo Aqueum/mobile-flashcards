81f52d8a46aba60fcf45951f833d44ab53600fd3
Aqueum <martin.currie@aqueum.com>
2018-05-10 22:00:34 +0100

    refactor: DeckAdd styles

--- src/components/DeckAdd.js
+++ src/components/DeckAdd.js
@@ -1,39 +1,10 @@
 import React, { Component } from 'react';
 import { TouchableOpacity, TextInput } from 'react-native';
-import styled from 'styled-components';
 import navigationOptions from 'react-navigation';
 import { connect } from 'react-redux';
 import { saveDeckTitle } from '../utils/api';
 import { addDeck } from '../actions';
-
-// styling
-const Page = styled.View`
-  flex: 1;
-`;
-
-const Prompt = styled.Text`
-  color: black;
-  font-size: 18;
-  text-align: left;
-  margin-left: 5;
-  margin-top: 5;
-`;
-
-const Input = styled.TextInput`
-  height: 40;
-  border-color: gray;
-  border-width: 1;
-  margin: 10px;
-  padding: 10px;
-`;
-
-const Submit = styled.Text`
-  color: blue;
-  font-size: 18;
-  text-align: right;
-  margin-right: 5;
-  margin-top: 5;
-`;
+import { Page, Prompt, Input, Submit } from '../style';
 
 // Page where new deck title is entered to create a new deck
 class DeckAdd extends Component {
